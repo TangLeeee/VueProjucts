c9ad0871ec4495e43366f545c4d383c6f7017850
xcy <779090286@qq.com>
2019-05-28 18:03:55 +0800

    暂时不继续更新，相关学习内容结束，app放在新的仓库中

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,40 @@
     <router-link to="/account">account</router-link>
     <router-link to="/goodlist">goodlist</router-link>
     <router-view></router-view>
+    <mt-button @click="show">button</mt-button>
+    <button type="button" class="mui-btn mui-btn-royal">
+      紫色
+    </button>
   </div>
 </template>
 
 <script>
+import { Toast } from 'mint-ui'
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      toastInstance: null
+    }
+  },
+  created () {
+    // this.getlist()
+  },
+  methods: {
+    getlist () {
+      this.show()
+      setTimeout(() => this.toastInstance.close(), 3000)
+    },
+    show () {
+      this.toastInstance = Toast({
+        message: '爱你mua~',
+        duration: 1500,
+        position: 'top',
+        iconClass: 'glyphicon glyphicon-heart',
+        className: 'mytoast'
+      })
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import app from './App'
 import VueRouter from 'vue-router'
 import router from './router/index'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import './css/app.css'
+import './lib/mui/css/mui.css'
 
 Vue.use(VueRouter)
-
+Vue.use(MintUI)
 // eslint-disable-next-line no-new
 new Vue({
   el: '#app',
